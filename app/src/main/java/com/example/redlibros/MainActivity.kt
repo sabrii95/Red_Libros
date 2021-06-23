86060926a12dc73b167751654a16de11e8d5c724
sabrii95 <sabrina.cabral1995@gmail.com>
2021-06-22 22:52:25 -0300

    "agregado de login"

--- app/src/main/java/com/example/redlibros/MainActivity.kt
+++ app/src/main/java/com/example/redlibros/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
